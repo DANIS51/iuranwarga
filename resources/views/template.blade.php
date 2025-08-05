a762b42ee894cbd6dbe4f07f375e76332aabafe9
Sajidan03 <rifansyahsajidan03@gmail.com>
2025-08-05 11:29:44 +0700

    tampilan

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -44,16 +44,11 @@
 <body>
 
   @include('navbar')
-
-
-
-    <footer>
-        <p>&copy; 2025 My CD danis maulid</p>
-    </footer>
- 
   @yield('content')
 
-
+  <foote class="text-center text-white mt-5" style="background-color: #1e2140;">
+      <p>&copy; 2025 My CD danis maulid</p>
+  </footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
